ff699dbd821aee0d0e82997aa1ea481044f59004
zhangxiaoyang <zhangxiaoyang.hit@gmail.com>
2016-03-29 15:25:12 +0800

    Update filter_img.sh

--- data-aggregator/bin/filter_img.sh
+++ data-aggregator/bin/filter_img.sh
@@ -3,4 +3,5 @@
 readonly finaldb_cut="$1"
 readonly imglist="$2"
 
-grep -o "http://[a-zA-Z0-9]*\.douban\.com/view/group_topic/large/public/[a-zA-Z0-9]*\.jpg" "$finaldb_cut" | sort | uniq > "$imglist"
+#grep -o "http://[a-zA-Z0-9]*\.douban\.com/view/group_topic/large/public/[a-zA-Z0-9]*\.jpg" "$finaldb_cut" | sort | uniq > "$imglist"
+egrep -o 'http[s]?://[^\S:]*?/\w*?\.jpg' "$finaldb_cut" | sort | uniq > "$imglist"
